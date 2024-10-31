95e298ac70cd2c887a7208d5bec02446a98e3068
Dawood Mohammed <dawoodmohammed919@gmail.com>
2024-10-31 10:47:16 +0200

    initializing...

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Path filePath = Paths.get("src","file.txt");
+
+        try (Stream<String> lines = Files.lines(filePath)) {
+            lines.forEach(System.out::println);
+        } catch (IOException e) {
+            System.err.println("An error occurred while reading the file: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
